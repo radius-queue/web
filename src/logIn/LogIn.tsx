f6f5eb5539693a6cf00e5be573166a67da59c5bb
grantwilliams <grantmw@uw.edu>
2020-07-01 16:53:30 -0700

    Adjusted indentation and added comments to fit eslint.

--- src/logIn/LogIn.tsx
+++ src/logIn/LogIn.tsx
@@ -8,41 +8,46 @@ interface logInValues {
 
 /**
  * Renders a page for logging into the business-side web app
+ * @return {html} Form with inputs for email, input for password,
+ *    and submit button
  */
 const BusinessLogInPage = () => {
-    const [formValues, setFormValues] = useForm({email:'', password:''});
+  const [formValues, setFormValues] = useForm({email: '', password: ''});
 
-    return (
-        <div className="form">
-            <input 
-                type="text"
-                name="email"
-                value={formValues.email}
-                placeholder="Business E-mail"
-                onChange={setFormValues}
-            />
-            <input 
-                type="password"
-                name="password"
-                value={formValues.password}
-                placeholder="Password"
-                onChange={setFormValues}
-            />
-            <button onClick={() => submitFormValues(
-                {
-                    email: formValues.email,
-                    password: formValues.password
-                }
-            )}>Log In</button>
-        </div>
-    )
-}
+  return (
+    <div className="form">
+      <input
+        type="text"
+        name="email"
+        value={formValues.email}
+        placeholder="Business E-mail"
+        onChange={setFormValues}
+      />
+      <input
+        type="password"
+        name="password"
+        value={formValues.password}
+        placeholder="Password"
+        onChange={setFormValues}
+      />
+      <button onClick={() => submitFormValues(
+          {
+            email: formValues.email,
+            password: formValues.password,
+          },
+      )}>Log In</button>
+    </div>
+  );
+};
 
 /**
  * Validates the given username and password.
+ * @param {logInValues} formValues Email and password entered by the
+ *    user when logging in
  */
 const submitFormValues = (formValues : logInValues) => {
-    console.log([formValues.email, formValues.password]);
-}
+  // TODO: Implement function (console.log is a placeholder)
+  console.log([formValues.email, formValues.password]);
+};
 
 export default BusinessLogInPage;
