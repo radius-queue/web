dd71f8e3cc31d6d98af274a5f115eff3c787f067
grantwilliams <grantmw@uw.edu>
2020-07-01 22:23:57 -0700

    Added link to registration page.

--- src/logIn/log-in.tsx
+++ src/logIn/log-in.tsx
@@ -4,6 +4,15 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
 interface logInValues {
     email: string;
     password: string;
@@ -18,39 +27,47 @@ const BusinessLogInPage = () => {
   const [formValues, setFormValues] = useForm({email: '', password: ''});
 
   return (
-    <Card id="login-card">
-      <h1 className="form-header">Log In!</h1>
-      <Form>
-        <Form.Group controlId="businessLogInEmail">
-          <Form.Label>Email Address:</Form.Label>
-          <Form.Control
-            type="text"
-            name="email"
-            value={formValues.email}
-            placeholder="account@example.com"
-            onChange={setFormValues}
-          />
-        </Form.Group>
-        <Form.Group controlId="businessLogInPassword">
-          <Form.Label>Password:</Form.Label>
-          <Form.Control
-            type="password"
-            name="password"
-            value={formValues.password}
-            placeholder="Password"
-            onChange={setFormValues}
-          />
-        </Form.Group>
-        <Button onClick={() => submitFormValues(
-            {
-              email: formValues.email,
-              password: formValues.password,
-            },
-        )} block>
-          Log In
-        </Button>
-      </Form>
-    </Card>
+    <div id="login-container">
+      <Card id="login-card">
+        <h1 className="form-header">Sign in to Ahead for Business</h1>
+        <Form>
+          <Form.Group controlId="businessLogInEmail">
+            <Form.Label>Email Address:</Form.Label>
+            <Form.Control
+              type="text"
+              name="email"
+              value={formValues.email}
+              placeholder="account@example.com"
+              onChange={setFormValues}
+            />
+          </Form.Group>
+          <Form.Group controlId="businessLogInPassword">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              value={formValues.password}
+              placeholder="Password"
+              onChange={setFormValues}
+            />
+          </Form.Group>
+          <Button onClick={() => submitFormValues(
+              {
+                email: formValues.email,
+                password: formValues.password,
+              },
+          )} block>
+            Log In
+          </Button>
+        </Form>
+      </Card>
+      <div>
+        <p>
+          New to Ahead?
+          <Link to="../../register/business-register"> Register here.</Link>
+        </p>
+      </div>
+    </div>
   );
 };
 
