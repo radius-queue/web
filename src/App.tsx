4a85ef8317947afc759138ab8e88ed71df784bd9
Zizhen Song <zizhensong@gmail.com>
2020-07-01 21:07:52 -0700

    standardization of files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {QueueList} from './QueueView/QueueView';
 import {TEST_QUEUE} from './util/HardcodedData';
-import RegistrationPage from './register/business-register';
+// import RegistrationPage from './register/business-register';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 /**
-<<<<<<< HEAD
  * Top Level app component
  *
  * @return {HTMLElement} App HTML
@@ -13,17 +12,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <QueueList queue={TEST_QUEUE} />
-=======
- * Presents the Business-Side Web App.
- * @return {html} Web App.
- */
-function App() {
-  return (
-    <div>
-      <BusinessLogInPage/>
-      <RegistrationPage/>
-    </div>
->>>>>>> 001df634b1bf5608c0f016b29ebd065f2ea50132
   );
 }
 
--- src/util/HardcodedData.ts
+++ src/util/HardcodedData.ts
@@ -1,4 +1,4 @@
-import {Queue, Party} from './Queue';
+import {Queue, Party} from './queue';
 
 export const TEST_QUEUE : Queue = new Queue('Test', undefined,
     [1, 2, 3].map((val) => new Party(val.toString(), val, val.toString(), val)));
