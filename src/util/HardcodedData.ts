a471c0a8e743b1952ee81c5f072dabca7898284e
grantwilliams <grantmw@uw.edu>
2020-07-01 21:51:02 -0700

    Adjusted links to account for file renaming.

--- src/util/HardcodedData.ts
+++ src/util/HardcodedData.ts
@@ -1,4 +1,4 @@
-import {Queue, Party} from './queue';
+import {Queue, Party} from './Queue';
 
 export const TEST_QUEUE : Queue = new Queue('Test', undefined,
     [1, 2, 3].map((val) => new Party(val.toString(), val, val.toString(), val)));
