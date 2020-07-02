e52ae5eebc3a3159debaae9870709ca4fc860422
wesleyplam <67298266+wesleyplam@users.noreply.github.com>
2020-07-01 18:11:44 -0700

    business registration form

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import BusinessLogInPage from './logIn/log-in';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import RegistrationPage from './register/business-register';
 
 function App() {
   return (
     <div className="App">
       <BusinessLogInPage/>
+      <RegistrationPage/>
     </div>
   );
 }
