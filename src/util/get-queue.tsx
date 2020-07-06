590b380d453bbe3d0ee931b225e6e1f2be4e3002
Zizhen Song <zizhensong@gmail.com>
2020-07-06 11:12:32 -0700

    addressed async behavior of getQueue

--- src/util/get-queue.tsx
+++ src/util/get-queue.tsx
@@ -4,21 +4,22 @@ import {firestore} from '../firebase';
 /**
  * @param uid
  */
-export default function getQueue(uid : string) {
-  firestore.collection('queues').doc(uid)
+export default async function getQueue(uid : string) {
+  let ret: Queue | undefined;
+  await firestore.collection('queues').doc(uid)
       .withConverter(queueConverter)
       .get().then(function(doc) {
         if (doc.exists) {
           const q: Queue | undefined = doc.data();
           // Use a City instance method
-          console.log(q);
-          return q;
+          ret = q;
         } else {
           console.log('No such document!');
         }
       }).catch(function(error) {
         console.log('Error getting document:', error);
       });
+  return ret;
 }
 
 
