5b5af0506022cd04b9be9477e9dcedc10188228e
grantwilliams <grantmw@uw.edu>
2020-07-20 18:37:38 -0700

    Landing page restructuring for routing revamp.

--- src/pre-log-in/landing-page.tsx
+++ src/pre-log-in/landing-page.tsx
@@ -1,48 +1,12 @@
 import React from 'react';
-import OurTeamPage from './our-team';
 import './landing-page.css';
-
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-
-import {
-  Link,
-  Switch,
-  Route,
-  useRouteMatch,
-} from 'react-router-dom';
 
 /**
  * the landing page for our website.
  *
  * @return {jsx} the HTML to be displayed for this component.
  */
-const LandingPage = () => {
-  const {path, url} = useRouteMatch();
 
-  return (
-    <div>
-      <Navbar id="landing-page-header">
-        <Link id='the-title' to="/">Radius</Link>
-        <Nav id='landing-page-links'>
-          <Link to={`${url}/our-team`}>Our Team</Link>
-          <Link to="/pre-log-in/register">Register</Link>
-          <Link to="/post-log-in/hub">Log In</Link>
-        </Nav>
-      </Navbar>
-      <div>
-        <Switch>
-          <Route path={`${path}/our-team`}>
-            <OurTeamPage />
-          </Route>
-          <Route path={`${path}`}>
-            <DefaultLandingPage />
-          </Route>
-        </Switch>
-      </div>
-    </div>
-  );
-};
 
 const DefaultLandingPage = () => {
   return (
@@ -58,4 +22,4 @@ const DefaultLandingPage = () => {
   );
 };
 
-export default LandingPage;
+export default DefaultLandingPage;
