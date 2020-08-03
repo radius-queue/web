ca4eaa28ac4dd3de884dd4886e6156fed95e9f88
wesleyplam <67298266+wesleyplam@users.noreply.github.com>
2020-08-02 22:21:11 -0700

    animations for landing page (plz ignore for now...)

--- src/pre-log-in/landing-page.tsx
+++ src/pre-log-in/landing-page.tsx
@@ -7,15 +7,20 @@ import './landing-page.css';
  * @return {jsx} the HTML to be displayed for this component.
  */
 
-
 const DefaultLandingPage = () => {
   return (
     <div id="landing-page-display">
-      <img
-        className="d-block w-100"
-        src="../../images/storefront-radii-dark.png"
-        alt="Businesses with Radii"
-      />
+      <div>
+        <img
+          className="hero-image"
+          src="../../images/storefront-radii-dark.png"
+          alt="Businesses with Radii"
+        />
+        <div className="hero-text">
+          <h1>Radius</h1>
+          <p>The waiting room reimagined.</p>
+        </div>
+      </div>
       <div id="landing-page-display-text">
         <h1>Radius for Business</h1>
         <p>Welcome to a new era of waiting rooms.</p>
